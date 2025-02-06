76997dec929d16ed8fa0a9b8e4178c9d966a7e9c
Guillaume Ferland <ferland182@gmail.com>
2025-02-06 11:05:09 -0500

    Add missing relationships and basic validations for models

--- app/models/status_change.rb
+++ app/models/status_change.rb
@@ -1,4 +1,8 @@
 class StatusChange < ApplicationRecord
   belongs_to :project
   belongs_to :user
+
+  enum status: { pending: 0, in_progress: 1, completed: 2, archived: 3 }
+
+  validates :status, presence: true
 end
